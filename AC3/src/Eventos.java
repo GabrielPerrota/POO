c74e4dbaeb88256568b157f63dfd2fd08a6ab217
GabrielPerrota <gabrielperrota2003@gmail.com>
2023-08-25 11:52:35 -0300

    AC3

--- AC3/src/Eventos.java
+++ AC3/src/Eventos.java
@@ -15,6 +15,27 @@ public class Eventos {
 
 
   }
-  
-  
+
+  void cadastrarEvento(String nome, String data, String local, int nummaxparticipantes, String obs){
+    
+    this.nome = nome;
+    this.data = data;
+    this.local = local;
+    this.nummaxparticipantes = nummaxparticipantes;
+    this.obs = obs;
+
+  }
+  void cadastrarAluno(Alunos string, int novamatricula){
+    string.matricula = novamatricula;
+    string.nome = string;
+
 }
+
+public void cadastrarAluno(String string, int novamatricula) {
+}
+
+
+}
+  
+  
+
